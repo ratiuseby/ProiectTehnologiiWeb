b52971a6661962f768709e1a8e07119be07ceac0
Unknown <pintiliciucoctavian@gmail.com>
2018-01-13 20:25:27 +0200

    login changes/generated register component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,12 +20,14 @@ import { HomeComponent, NavbarComponent } from './components';
 import { fakeBackendProvider } from './service';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions } from '@angular/http';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     AppRoutingModule,
